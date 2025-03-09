4519de4d5b3d83662ee6fb4024d0fc0383a8e95a
Rami Khawaly <rami.khawaly@gmail.com>
2025-03-09 16:12:06 +0200

    added assets file

---
+++ lib/config/assets.dart
@@ -0,0 +1,6 @@
+// assets.dart
+class Assets {
+  static const String blazingFalconsLogo = 'assets/images/blazing_falcons.png';
+  static const String shadowStrikersLogo = 'assets/images/shadow_strikers.png';
+  // Add other image paths as needed
+}
